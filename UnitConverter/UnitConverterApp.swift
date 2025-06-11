a54e0d4d55a4caa836a1d1a1113b00c6b0d3bc35
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-11 23:08:55 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Gurur on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
